884a472914f941424252d520cacc7d739d141ae8
Karol <Karol@DESKTOP-TIIJ6VQ>
2020-11-27 06:14:22 +0100

    Dodaj pliki projektów.

---
+++ zad3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static IEnumerable<IEnumerable<string>> OnlyBigCollections(List<IEnumerable<string>> toFilter)
+        {
+            Func<IEnumerable<string>, bool> predicate = 
+                list => list.ElementAtOrDefault(5) != null;
+            //  predicate = list => list.Count() >5 ;
+
+            return toFilter.Where(predicate);
+        }
+    }
+}
